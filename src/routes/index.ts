3668395dc0e297be6d5171766d385c06ff0dc56c
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-09-29 15:22:04 -0400

    :sparkles: create appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.route';
 
 const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;

3ce8312eb9c19f829d3c186f6bfe88c0d913d0e5
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-10-02 16:37:40 -0400

    :sparkles: create users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.route';
+import usersRouter from './users.route';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-
+routes.use('/users', usersRouter);
 export default routes;
